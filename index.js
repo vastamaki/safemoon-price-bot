02004983950a24fb54c6bfd5a3b70a53b203253b
Viljami Vastamäki <viljami.vastamaki@tutanota.com>
2021-12-04 17:46:02 +0200

    Some changes

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 const TelegramBot = require("node-telegram-bot-api");
 const { token, groupId, groupName } = require("./secrets.json");
-const fetch = require("cross-fetch");
+
+const { getPrice } = require("./price");
 
 const bot = new TelegramBot(token, { polling: false });
 
 const updatePrice = async () => {
-  const res = await fetch(
-    `https://api.pancakeswap.info/api/v2/tokens/0x8076c74c5e3f5852037f31ff0093eeb8c8add8d3`
+  const web3price = await getPrice();
+  const price = parseFloat(web3price).toFixed(8);
+
+  const date = new Date(
+    new Date().toLocaleString("en-US", {
+      timeZone: "Europe/Helsinki",
+    })
   );
-  const { data } = await res.json();
 
-  const price = parseFloat(data.price).toFixed(8);
+  const minutes =
+    date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
 
-  const date = new Date(new Date().toLocaleString("en-US", {
-    timeZone: "Europe/Helsinki",
-  }));
-
-  const minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
-
-  await bot.setChatTitle(groupId, `${groupName} (${price.replace("0.00000", "")}, ${date.getHours()}:${minutes})`);
+  await bot.setChatTitle(
+    groupId,
+    `${groupName} (${price.replace(
+      "0.00000",
+      ""
+    )}, ${date.getHours()}:${minutes})`
+  );
 };
 
 updatePrice();
